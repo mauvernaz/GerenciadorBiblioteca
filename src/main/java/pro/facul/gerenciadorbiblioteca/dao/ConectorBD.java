7c8d52ab53d75660467fe46ca23af43c489da49b
Matheus Alvernaz <msalvernaz13@gmail.com>
2025-11-12 15:52:37 -0300

    add feature de usuario com banco

--- src/main/java/pro/facul/gerenciadorbiblioteca/dao/ConectorBD.java
+++ src/main/java/pro/facul/gerenciadorbiblioteca/dao/ConectorBD.java
@@ -1,4 +1,23 @@
 package pro.facul.gerenciadorbiblioteca.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConectorBD {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/gerenciador_biblioteca";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException("Erro de conexão com banco: " + e.getMessage(), e);
+        }
+    }
 }
--- src/main/java/pro/facul/gerenciadorbiblioteca/dao/UsuarioDAO.java
+++ src/main/java/pro/facul/gerenciadorbiblioteca/dao/UsuarioDAO.java
@@ -1,4 +1,44 @@
 package pro.facul.gerenciadorbiblioteca.dao;
 
+import pro.facul.gerenciadorbiblioteca.model.Usuario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UsuarioDAO {
+
+    public Usuario validarLogin(String email, String senha) {
+
+        String sql = "SELECT * FROM Usuario WHERE email = ? AND senha = ?";
+
+        Usuario usuario = null;
+
+        try (Connection conn = ConectorBD.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, email);
+            stmt.setString(2, senha);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+
+                if (rs.next()) {
+                    usuario = new Usuario();
+                    usuario.setId(rs.getInt("id"));
+                    usuario.setNome(rs.getString("nome"));
+                    usuario.setEmail(rs.getString("email"));
+                    usuario.setTipo(rs.getString("tipo"));
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao validar login: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return usuario;
+    }
+
+    //crud...
 }
